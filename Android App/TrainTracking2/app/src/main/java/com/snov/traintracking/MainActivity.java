6b2ebbd48315e3c3c3ed4f1ad08971207192b37f
shamalka <shamalkanavod95@gmail.com>
2018-08-29 19:42:15 +0530

    Home UI Routing

--- Android App/TrainTracking2/app/src/main/java/com/snov/traintracking/MainActivity.java
+++ Android App/TrainTracking2/app/src/main/java/com/snov/traintracking/MainActivity.java
@@ -1,14 +1,31 @@
 package com.snov.traintracking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView TrackingCard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Test01");
+
+        TrackingCard = (CardView)findViewById(R.id.TrainTrackingView);
+
+        TrackingCard.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intent = new Intent(MainActivity.this, TrackingActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

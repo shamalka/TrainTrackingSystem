ea754eb8ea841233d36e9a761695e0b02459ffbd
vethiga <vethigasatgunarajah@gmail.com>
2019-01-04 15:11:12 +0530

    AppUI's

---
+++ Application UI's/AndroidApplication/app/src/main/java/com/example/hp/newapplication/ReseActivity.java
@@ -0,0 +1,13 @@
+package com.example.hp.newapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rese);
+    }
+}
